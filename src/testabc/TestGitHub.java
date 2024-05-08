859086ee3493a491795678f553b0e5bbfbd9c8dc
admin <jamesnguyen0102@gmail.com>
2024-05-08 11:07:38 +0700

    main commit

--- src/testabc/TestGitHub.java
+++ src/testabc/TestGitHub.java
@@ -3,19 +3,9 @@ package testabc;
 public class TestGitHub {
 
     public void main(){
-        System.out.println("sua truc tiep tu git - nhanh main");
-        System.out.println("jasmine");
-        System.out.println("edit tren main111111");
+        System.out.println("TNT");
 
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
 }
